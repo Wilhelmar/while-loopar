75697ce44f60fad88e74db9c35cc287677a5c7e4
wilhelmar <wilhelmar@vgy.se>
2016-09-23 15:12:08 +0200

    Gjorde alla övningar fram till 2.5 där jag inte hann mer på lektionen!

--- while_satsen/Program.cs
+++ while_satsen/Program.cs
@@ -10,6 +10,119 @@ namespace while_satsen
     {
         static void Main(string[] args)
         {
+            /*
+
+            //Övning 1
+            Console.WriteLine("Här kommer vi se en demonstration av While-loopen!");
+            int i = 0;
+
+            while (i++ < 100)
+            {
+                Console.WriteLine(i);
+            }
+
+   
+
+            //Övning 2
+
+
+            
+
+            while (true)
+            {
+                Console.Write("Skriv in ditt lösenord: ");
+                string password = Console.ReadLine();
+
+                if (password == "Wilhelmar") 
+                {
+                    Console.WriteLine("Välkommen!");break;
+                }
+               
+                else
+                {
+                    Console.WriteLine("Du skrev fel lösenord!");
+                }
+            }
+         
+  
+
+            //Övning 2.1
+
+            var i = 0;
+            while (i++ < 100)
+            {
+                Console.WriteLine(i);
+            }       
+
+   
+
+            //Övning 2.2
+
+            var i = 10;
+            while (i-- > 1)
+            {
+                Console.WriteLine(i);
+            }
+
+  
+
+            //Övning 2.3
+
+            while (true)
+            {
+                Console.WriteLine("Jag är en oändlig loop! Avbryt mig med Ctrl + C");
+            }
+
+
+
+            //Övning 2.4
+
+                Console.WriteLine("Vilket tal mellan 1 och 100 tänker jag på?");
+                
+            
+
+                Random rnd = new Random();
+
+                bool vilkor = true;
+
+                int tal = rnd.Next(100);
+                int i = 0;
+
+                while (true)
+            {
+                
+                int guess = int.Parse(Console.ReadLine());
+                
+                if (guess > tal )
+                {
+                    Console.WriteLine("För högt! Gissa lägre!"); break;
+                }
+
+                else if (guess < tal )
+                {
+                    Console.WriteLine("För lågt! Gissa högre");
+                }
+
+                else 
+                {
+                    Console.WriteLine("Det är rätt! Grattis!!");
+                    vilkor = false;
+                }
+                i++;
+                
+            }
+            Console.ReadLine();
+
+*/
+            //Övning 2.5
+
+
+
+
+
+
+         
+
         }
     }
 }
